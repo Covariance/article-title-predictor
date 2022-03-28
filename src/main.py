146743702fd30a8dd5312dfd9fa58a4db71d4cf7
Pavel Martynov <covariancemomentum@gmail.com>
2022-03-28 18:40:26 +0300

    Model added

--- src/main.py
+++ src/main.py
@@ -1,11 +1,57 @@
+from os import getenv
+
 import streamlit as st
+import numpy as np
+from transformers import DistilBertForSequenceClassification, AutoTokenizer
+
+@st.cache
+def load_model(path = getenv('MODEL_PATH')):
+    return DistilBertForSequenceClassification.from_pretrained('./model', local_files_only=True)
+
+@st.cache
+def load_tokenizer():
+    return AutoTokenizer.from_pretrained('distilbert-base-cased')
+
+
+tokenizer = load_tokenizer()
+model = load_model()
+categories = np.array([
+    'biology', 
+    'computer_science',
+    'economics',
+    'electrical_engineering',
+    'finance',
+    'mathematics',
+    'physics',
+    'statistics',
+    'unknown'
+])
+
+def run_model(input_text: str, border: float = 0.95):
+    tokens = tokenizer.encode(input_text, truncation=True, padding=True, return_tensors='pt')
+    preds = model(tokens)
+    [probs] = preds.logits.softmax(dim=-1).tolist()
+
+    idx = np.flip(np.argsort(probs))
+
+    total = 0.
+    result = []
+
+    for cat, prob in zip(categories[idx], sorted(probs)):
+        result.append((cat, prob))
+        total += prob
+        if total >= border:
+            return result
 
 st.write('''
-Hello, StreamLit!
+# Hello, StreamLit!
 ''')
 
-ans = st.text_area('Say hello:')
+input_text = st.text_area('Input abstract of an article you want to classify:')
+
+result = run_model(input_text)
 
 st.write(f'''
-{ans}
+### The article is:
+{result}
 ''')
