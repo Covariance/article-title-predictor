272872b431ae77ca4fb37c2682a1168820c4a0a5
Pavel Martynov <covariancemomentum@gmail.com>
2022-03-28 18:57:58 +0300

    Unhashability solved

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ import streamlit as st
 import numpy as np
 from transformers import DistilBertForSequenceClassification, AutoTokenizer
 
-@st.cache
+@st.cache(allow_output_muatation=True)
 def load_model(path = getenv('MODEL_PATH')):
     return DistilBertForSequenceClassification.from_pretrained('./model', local_files_only=True)
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_tokenizer():
     return AutoTokenizer.from_pretrained('distilbert-base-cased')
 
